e9a3ddeda14269c8f86d9afb824ec87717f19e5b
ibrahim mohamed <ibrahim.mohamed201537@mail.com>
2023-03-17 17:03:55 +0200

    ex1

---
+++ unit2-c-programming/lecture-3-assignments/EX1.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : EX1.c
+ Author      :ibrahim mohamed
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	printf("C Programming\n");
+	return 0;
+}
